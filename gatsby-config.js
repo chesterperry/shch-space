aa5f837c7269a5d40d22591e677b2fccbec26493
chesterperry <oleg.shch@gmail.com>
2025-01-14 15:23:21 +0300

    +

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
           "type": "service_account",
           "project_id": "oleg-scherbinin-project",
           private_key_id: process.env.PRIVATE_KEY_ID,
-          private_key: process.env.PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n'),
+          private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
           "client_email": "oleg-personal-gatsby-data@oleg-scherbinin-project.iam.gserviceaccount.com",
           "client_id": "113342692822999418879",
           "auth_uri": "https://accounts.google.com/o/oauth2/auth",
